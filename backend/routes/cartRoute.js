f99fd854331fcedf4edca7497872a9842048e51e
Godswill199 <godswill199@gmail.com>
2021-01-05 22:27:50 +0100

    just frontend

--- backend/routes/cartRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router =  express.Router();
-const cartController = require("../controller/cartController")
-
-router.post("/cart", cartController.addItemToCart)
-router.get("/cart", cartController.getCart);
-router.delete("/empty-cart", cartController.emptyCart)
-  module.exports = router;
-  
