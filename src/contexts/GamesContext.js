b2ac004c5d7581c9a1ac969d80386bc0d86550e4
Godswill <51128320+Godswill199@users.noreply.github.com>
2021-03-12 13:00:21 +0100

    Change game price

--- src/contexts/GamesContext.js
+++ src/contexts/GamesContext.js
@@ -16,7 +16,7 @@ const GamesContextProvider = (props) => {
         {id: 1, image: game1, title: 'golden jackpot', price: 2000.00, category: 1, isNew: true, topPrize: "N5,000,000", isVerOrient: true,
         desc: "Scratch only 4 cards, if you find the jackpot card, you win N5,000,000. If you match 4 identical cards you win N50,000 instantly!"},
 
-        {id: 2, image: game2, title: 'find the ball', price: 3000.00, category: 2, isNew: true, topPrize: "N50,000", isVerOrient: false,
+        {id: 2, image: game2, title: 'find the ball', price: 5000.00, category: 2, isNew: true, topPrize: "N50,000", isVerOrient: false,
         desc: "Scratch only 1 box to reveal the ball, if you find the ball, you win N50,000 instantly!"},
 
         {id: 3, image: game3, title: 'football club search', price: 500.00, category: 2, isNew: false, topPrize: "N100,000", isVerOrient: false,
@@ -25,7 +25,7 @@ const GamesContextProvider = (props) => {
         {id: 4, image: game4, title: 'Gadget Hunt', price: 3000.00, category: 1, isNew: false, topPrize: "N500,000", isVerOrient: true,
         desc: "Scratch only four circles to reveal gadgets, If you have four matching gadgets you win!"},
 
-        {id: 5, image: game5, title: 'Seasons Greetings', price: 5000.00, category: 3, isNew: false, topPrize: "1,000,000", isVerOrient: true,
+        {id: 5, image: game5, title: 'Seasons Greetings', price: 3000.00, category: 3, isNew: false, topPrize: "1,000,000", isVerOrient: true,
         desc: "Scratch only three(3) Christmas tree balls & win any amount you find!"},
 
         {id: 6, image: game6, title: 'Sure Banker', price: 1000.00, category: 3, isNew: false, topPrize: "N50,000", isVerOrient: true,
